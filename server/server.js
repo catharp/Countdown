e9e807f36bf8e9f19bac8b5bfe5447cbfec3d6e6
Collin Tharp <catharp@gmail.com>
2017-02-10 12:09:12 -0800

    [MODIFY] serve client at root path {CT}

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/../client'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log('listening on port', PORT);

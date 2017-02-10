f6de0775bbd570726f2dd56a963ef80f0415d25f
Collin Tharp <catharp@gmail.com>
2017-02-10 13:15:11 -0800

    [INIT] create templateUrl for clock component {CT}

--- client/countdown.js
+++ client/countdown.js
@@ -1,6 +1,6 @@
 angular.module('countdown', [])
 .component('clock', {
-  template: 'Time:  {{ $ctrl.time }}',
+  templateUrl: 'clock.html',
   controller: function($timeout) {
     var tick = () => {
       this.time = Date.now();
